4e41604ff2f60563e608f5ea18795100b94dda13
Valter f Carvalho <walterfcarvalho@gmail.com>
2023-05-05 23:04:20 +0100

    config firebase app

---
+++ src/api/config.tsx
@@ -0,0 +1,10 @@
+//firebaseAppKey
+export const firebaseConfig = {
+  apiKey: "AIzaSyDr5n-LKnmavyGosqK9FjJadkHM4CAG9Og",
+  authDomain: "hmonitor-57097.firebaseapp.com",
+  projectId: "hmonitor-57097",
+  storageBucket: "hmonitor-57097.appspot.com",
+  messagingSenderId: "1038874301708",
+  appId: "1:1038874301708:web:213fe7a3fd63e4a93da529",
+  measurementId: "G-THD2K9C2R0"
+}
